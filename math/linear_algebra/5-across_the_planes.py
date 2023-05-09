c7c529b1423b956e5650ed3d76399e3d6367f655
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-09 14:26:58 -0500

    [Added]: a fcuntion that adds two matrices element-wise.

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that adds two matrices element-wise.
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two matrices element-wise.
+
+    Args:
+        mat1 (list): A 2D matrix containing ints or floats.
+        mat2 (list): A 2D matrix containing ints or floats.
+
+    Returns:
+        A new 2D matrix that is the element-wise sum of mat1 and mat2,
+        or None if mat1 and mat2 are not the same shape.
+    """
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
+        return None
+    return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
