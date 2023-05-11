7bcde2effed56bb04658ddf884962f2f8f06bab6
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-11 18:33:36 -0500

    [Added]: a function that concatenates two arrays.

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function that concatenates two arrays.
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+
+    Args:
+        arr1 (list): A list of ints or floats.
+        arr2 (list): A list of ints or floats.
+
+    Returns:
+        A new list that is the concatenation of arr1 and arr2.
+    """
+    return arr1 + arr2
