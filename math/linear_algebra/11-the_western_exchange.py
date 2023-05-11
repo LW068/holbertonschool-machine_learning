6302f9ba5af9884e58474fcdc722e4edb8095815
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-11 18:53:07 -0500

    [Added]: a function that transposes a numpy.ndarray

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the function np_transpose that transposes
+a numpy.ndarray
+"""
+
+
+import numpy as np
+
+def np_transpose(matrix):
+    """
+    Function that transposes a numpy.ndarray
+    Args:
+        matrix (numpy.ndarray): The input array.
+    Returns:
+        numpy.ndarray: The transposed array.
+    """
+    return np.transpose(matrix)
