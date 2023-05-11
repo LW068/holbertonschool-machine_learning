2e9adaa669c8370d9f9a7e51e378a77034df4b52
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-11 18:51:34 -0500

    [Fixed]: pycodestyle validation.

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module contains the function np_shape that calculates the shape
+of a numpy.ndarray
+"""
+
+
 def np_shape(matrix):
     """
     Function that calculates the shape of a numpy.ndarray
+    Args:
+        matrix (numpy.ndarray): The input array.
+    Returns:
+        tuple: The shape of the input array.
     """
     return matrix.shape
