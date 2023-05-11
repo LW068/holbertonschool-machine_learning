9afbb4273f3264c148c5ad6d6947881059529e09
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-11 18:59:27 -0500

    [Added]: a function that performs element-wise addition, subtraction, multiplication, and division.2

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 """
-This module contains the function np_elementwise that performs element-wise
-addition, subtraction, multiplication, and division of two numpy.ndarrays
+This module contains the function np_elementwise that performs 
+element-wise addition, subtraction, multiplication, and division
+without using any loops or conditional statements.
 """
 
 
 def np_elementwise(mat1, mat2):
     """
-    Function that performs element-wise addition, subtraction,
-    multiplication, and division of two numpy.ndarrays
+    Function that performs element-wise operations on two numpy arrays.
     Args:
         mat1 (numpy.ndarray): The first input array.
         mat2 (numpy.ndarray): The second input array.
     Returns:
-        tuple: A tuple containing the element-wise sum, difference,
-        product, and quotient, respectively.
+        tuple: The element-wise sum, difference, product, and quotient 
+               of mat1 and mat2, respectively.
     """
-    addition = np.add(mat1, mat2)
-    subtraction = np.subtract(mat1, mat2)
-    multiplication = np.multiply(mat1, mat2)
-    division = np.divide(mat1, mat2)
-
-    return (addition, subtraction, multiplication, division)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
