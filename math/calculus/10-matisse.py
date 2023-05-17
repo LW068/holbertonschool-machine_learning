8a17bdf404fe20284713f7279fb6d8950658c64f
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-17 16:28:33 -0500

    [Fixed]: pycodestyle validation.

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -12,13 +12,17 @@ def poly_derivative(poly):
     poly (list): A list of coefficients representing a polynomial.
 
     Returns:
-    list: The coefficients of the derivative of the polynomial, or None if poly is not valid.
+    list: The coefficients of the derivative of the polynomial,
+          or None if poly is not valid.
     """
-    if not isinstance(poly, list) or len(poly) == 0 or not all(isinstance(i, (int, float)) for i in poly):
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+    if not all(isinstance(i, (int, float)) for i in poly):
         return None
     if len(poly) == 1:
         return [0]
     return [i*poly[i] for i in range(1, len(poly))]
+
 
 if __name__ == "__main__":
     poly = [5, 3, 0, 1]
