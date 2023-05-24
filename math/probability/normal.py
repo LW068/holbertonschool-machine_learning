e32586e9dfe14907bb8caada47dcb65a52e5fed4
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-24 14:18:15 -0500

    [Added]: a module for normal distribution.

---
+++ math/probability/normal.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Module for Normal distribution
+"""
+
+
+class Normal:
+    """
+    Class that represents a normal distribution
+    """
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Class constructor
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            else:
+                self.mean = sum(data) / len(data)
+                variance = sum([(x - self.mean) ** 2 for x in data]) / len(data)
+                self.stddev = variance ** 0.5
