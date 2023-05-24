7ef0398f00014a018aa4a97dc0e4ba756aca1807
LW068 <105394574+LW068@users.noreply.github.com>
2023-05-24 14:32:10 -0500

    [Added]: Binomial distribution class.

---
+++ math/probability/binomial.py
@@ -0,0 +1,26 @@
+class Binomial:
+    """
+    Class that represents a binomial distribution
+    """
+
+    def __init__(self, data=None, n=1, p=0.5):
+        """
+        Class constructor
+        """
+        if data is None:
+            if n <= 0:
+                raise ValueError('n must be a positive value')
+            if p <= 0 or p >= 1:
+                raise ValueError('p must be greater than 0 and less than 1')
+            self.n = int(n)
+            self.p = float(p)
+        else:
+            if type(data) is not list:
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            mean = sum(data) / len(data)
+            variance = sum((x - mean) ** 2 for x in data) / len(data)
+            p = 1 - variance / mean
+            self.n = round(mean / p)
+            self.p = mean / self.n
