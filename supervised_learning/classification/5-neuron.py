12c7fc3ddbf765277db6b31a499fd8248158f8ad
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-07 18:55:02 -0500

    [Added]: a class Neuron that defines a single neuron performing binary classification.

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+This module contains a class Neuron which defines a single neuron perf0rming
+binary classification.
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Class Neuron defining a single neuron perf0rming binary classification
+    """
+    def __init__(self, nx):
+        """
+        Initialize Neuron
+        """
+        if type(nx) != int:
+            raise TypeError('nx must be a integer')
+        if nx < 1:
+            raise ValueError('nx must be positive')
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        Getter f0r weights vector
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Getter f0r bias
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        Getter f0r activated output
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neuron’s predictions
+        """
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        prediction = np.where(A >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """
+        Calculates one pass of gradient descent on the neuron
+        """
+        m = X.shape[1]
+        dZ = A - Y
+        dW = np.dot(dZ, X.T) / m
+        db = np.sum(dZ) / m
+        self.__W = self.__W - alpha * dW
+        self.__b = self.__b - alpha * db
