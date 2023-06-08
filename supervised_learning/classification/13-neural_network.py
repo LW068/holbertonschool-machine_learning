30fe15c2736cf37e3efa0e63ca0932bcc98e5766
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-07 21:37:58 -0500

    [Fixed]: incomplete code.

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -107,4 +107,10 @@ class NeuralNetwork:
         dW2 = (1/m) * np.matmul(dZ2, A1.T)
         db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
         dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
-        d
+        dW1 = (1/m) * np.matmul(dZ1, X.T)
+        db1 = (1/m) * np.sum(dZ1, axis=1, keepdims=True)
+
+        self.__W1 = self.__W1 - alpha * dW1
+        self.__b1 = self.__b1 - alpha * db1
+        self.__W2 = self.__W2 - alpha * dW2
+        self.__b2 = self.__b2 - alpha * db2
