db05c340cef14153bb1c65e603a52c60d3e1be67
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-07 21:54:04 -0500

    [Added]: defines a deep neural network performing binary classification.

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+""" Module to create a DeepNeuralNetwork """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    DeepNeuralNetwork class
+    """
+    def __init__(self, nx, layers):
+        """ Class constructor """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list) or not layers:
+            raise TypeError('layers must be a list of positive integers')
+        if not all(map(lambda x: x > 0 and isinstance(x, int), layers)):
+            raise TypeError('layers must be a list of positive integers')
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.L):
+            if i == 0:
+                self.weights['W' + str(i+1)] = np.random.randn(layers[i], nx) * np.sqrt(2/nx)
+            else:
+                self.weights['W' + str(i+1)] = np.random.randn(layers[i], layers[i-1]) * np.sqrt(2/layers[i-1])
+            self.weights['b' + str(i+1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        """ Returns the number of layers """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Holds all intermediary values of the network """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Holds all weights and bias """
+        return self.__weights
