6b7021fe4100824dfa888e84800954999ab3ef66
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-15 16:26:52 -0500

    [Fixed]: imported module.

--- supervised_learning/classification/20-main.py
+++ supervised_learning/classification/20-main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import zipfile
 
 Deep = __import__('20-deep_neural_network').DeepNeuralNetwork
 
-lib_train = np.load('../data/Binary_Train.zip')
+zip_path = 'data/Binary_Train.zip'
+zip_file = zipfile.ZipFile(zip_path, 'r')
+npz_file = zip_file.open('Binary_Train.npz')
+lib_train = np.load(npz_file)
 X_3D, Y = lib_train['X'], lib_train['Y']
 X = X_3D.reshape((X_3D.shape[0], -1)).T
 
