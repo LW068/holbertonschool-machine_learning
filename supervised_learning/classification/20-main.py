444c9159e6e4186159fcaaa84a908b5ded375dbc
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-15 16:30:10 -0500

    [Fixed]: needed temp file to extract the content of the .npz

--- supervised_learning/classification/20-main.py
+++ supervised_learning/classification/20-main.py
@@ -2,15 +2,23 @@
 
 import numpy as np
 import zipfile
+import tempfile
 
 Deep = __import__('20-deep_neural_network').DeepNeuralNetwork
 
 zip_path = 'data/Binary_Train.zip'
 zip_file = zipfile.ZipFile(zip_path, 'r')
 npz_file = zip_file.open('Binary_Train.npz')
-lib_train = np.load(npz_file)
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
+
+# Create a temporary file and extract the content of the .npz file
+with tempfile.NamedTemporaryFile() as tmp_file:
+    tmp_file.write(npz_file.read())
+    tmp_file.flush()
+    
+    # Load the data using np.load
+    lib_train = np.load(tmp_file.name)
+    X_3D, Y = lib_train['X'], lib_train['Y']
+    X = X_3D.reshape((X_3D.shape[0], -1)).T
 
 np.random.seed(0)
 deep = Deep(X.shape[0], [5, 3, 1])
