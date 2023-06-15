c81ca9e81b9ae0b91a9e0a8ffe39045f90db8458
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-14 23:49:03 -0500

    Update 7-evaluate.py

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
-"""Evaluate the output of a neural network"""
+"""
+Evaluate the output of a neural network
+"""
 import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """Evaluate the output of a neural network"""
+    """function that evaluates the output of a nn"""
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path + ".meta")
-        saver.restore(sess, save_path)
-        graph = tf.get_default_graph()
-        x = graph.get_tensor_by_name("x:0")
-        y = graph.get_tensor_by_name("y:0")
-        y_pred = graph.get_tensor_by_name("y_pred:0")
-        loss = graph.get_tensor_by_name("loss:0")
-        accuracy = graph.get_tensor_by_name("accuracy:0")
-        y_pred = sess.run(y_pred, feed_dict={x: X, y: Y})
-        loss = sess.run(loss, feed_dict={x: X, y: Y})
-        accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
-    return y_pred, accuracy, loss
+        loader = tf.train.import_meta_graph(save_path + '.meta')
+        loader.restore(sess, save_path)
+
+        # Retrieve the required tensors from the graph's collection
+        var_names = ['x', 'y', 'y_pred', 'accuracy', 'loss']
+        for var_name in var_names:
+            # Get the tensor by name and assign it to a global variable
+            globals()[var_name] = tf.get_collection(var_name)[0]
+
+        # Run the evaluation operations
+        y_pred = sess.run(globals()['y_pred'], feed_dict={x: X, y: Y})
+        loss = sess.run(globals()['loss'], feed_dict={x: X, y: Y})
+        acc = sess.run(globals()['accuracy'], feed_dict={x: X, y: Y})
+
+    return y_pred, acc, loss
