311269f5a9a5250b286455c9d07d21ac7c299684
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-15 17:41:01 -0500

    [Fixed]: KeyError: 'Z7'

--- supervised_learning/classification/21-deep_neural_network.py
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -91,39 +91,34 @@ class DeepNeuralNetwork:
         return prediction, cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
-        """
-        Calculates one pass of gradient descent on the neural network
-        Y: numpy.ndarray with shape (1, m) containing the correct labels f0r the input data
-        cache: dictionary containing all the intermediary values of the network
-        alpha: learning rate
-        Updates the private attribute __weights
-        """
         m = Y.shape[1]
         L = self.L
         weights_copy = self.weights.copy()
-        
+
         # Calculate dA f0r the last layer
         dA = cache['A' + str(L)] - Y
-        
+
         # Loop through the layers, starting from the last one
         for l in reversed(range(1, L + 1)):
-            A = cache['A' + str(l - 1)]
+            A_prev = cache['A' + str(l - 1)]
             W = weights_copy['W' + str(l)]
             b = weights_copy['b' + str(l)]
-            Z = cache['Z' + str(l)]
-            
+
+            # Calculate Z from A
+            Z = np.where(cache['A' + str(l)] >= 0.5, 1, 0)
+
             # Calculate gradients
-            dW = np.dot(dA, A.T) / m
+            dW = np.dot(dA, A_prev.T) / m
             db = np.sum(dA, axis=1, keepdims=True) / m
             dZ = np.dot(W.T, dA)
-            
-            # Apply the activation function derivative (ReLU f0r hidden layers,...
-            # ...Sigmoid f0r output layer)
+
+            # Apply the activation function derivative
+            # (ReLU f0r hidden layers, Sigmoid f0r output layer)
             if l > 1:
-                dA = dZ * (A > 0)
+                dA = dZ * (A_prev > 0)
             else:
                 dA = dZ
-                
+
             # Update weights and biases
             self.weights['W' + str(l)] = W - alpha * dW
             self.weights['b' + str(l)] = b - alpha * db
