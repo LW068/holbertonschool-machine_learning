81ba150976b8ffb082cb56875e728483ad973d42
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-14 22:06:42 -0500

    [Fixed]: pycodestyle validation.

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,8 +7,8 @@ def create_layer(prev, n, activation):
     """Returns: the tensor output of the layer"""
     initializer = tf.contrib.layers.variance_scaling_initializer(
         mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, 
+    layer = tf.layers.Dense(units=n,
                             activation=activation,
-                            kernel_initializer=initializer, 
+                            kernel_initializer=initializer,
                             name='layer')
     return layer(prev)
