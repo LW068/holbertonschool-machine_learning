f04c00074d7a111e9f4a748a6ff20006d9c4b9dc
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-17 00:19:23 -0500

    [Revised]: attempt at fixing errors.

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -8,27 +8,30 @@ import numpy as np
 
 
 def one_hot_decode(one_hot):
-    """
-    Converts a one-hot matrix into a vector of labels.
+  """
+  Converts a one-hot matrix into a vector of labels.
+  Args:
+  - one_hot (numpy.ndarray): A one-hot encoded matrix...
+      ...with shape (classes, m).
+    classes is the maximum number of classes.
+    m is the number of examples.
 
-    Args:
-    - one_hot (numpy.ndarray): A one-hot encoded matrix...
-        ...with shape (classes, m).
-      classes is the maximum number of classes.
-      m is the number of examples.
+  Returns:
+  - numpy.ndarray: A vector with shape (m,) containing the numeric labels
+    for each example, or None on failure.
+  """
+  # Checking input types/shapes!
+  print("Input type:", type(one_hot))
+  print("Input shape:", one_hot.shape)
+  # Check if the input is valid!
+  if not isinstance(one_hot, np.ndarray):
+      print("Invalid input in one_hot_decode:", one_hot)
+      return None
 
-    Returns:
-    - numpy.ndarray: A vector with shape (m,) containing the numeric labels
-      for each example, or None on failure.
-    """
-    # Check if the input is valid!
-    if not isinstance(one_hot, np.ndarray):
-        print("Invalid input in one_hot_decode:", one_hot)
-        return None
-
-    try:
-        # Find the index of the maximum value in each column
-        labels = np.argmax(one_hot, axis=0)
-        return labels
-    except Exception:
-        return None
+  try:
+      # Find the index of the maximum value in each column
+      labels = np.argmax(one_hot, axis=0)
+      return labels
+  except Exception as e:
+      print("Error in one_hot_decode:", str(e))
+      return None
