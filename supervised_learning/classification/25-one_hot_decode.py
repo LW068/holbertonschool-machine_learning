af47dbb295350a1bcede46c50e135416ffd7408e
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-17 00:27:57 -0500

    [Fixed]: pycodestyle validation, whitespace and split line.

--- supervised_learning/classification/25-one_hot_decode.py
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -12,7 +12,8 @@ def one_hot_decode(one_hot):
     Converts a one-hot matrix into a vector of labels.
 
     Args:
-    - one_hot (numpy.ndarray): A one_hot encoded matrix with shape (classes, m).
+    - one_hot (numpy.ndarray): A one_hot encoded...
+    ...matrix with shape (classes, m).
         classes represents the maximum number of classes.
         m represents the number of examples.
 
@@ -20,7 +21,7 @@ def one_hot_decode(one_hot):
     - numpy.ndarray: A vector of shape (m,) containing the numeric labels
       for each example, or None on failure.
     """
-    
+  
     if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
         return None
 
