ec99904306f7d90cf45f7ebc992aeb7ad02287e2
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-26 19:26:47 -0500

    [Fixed]: pycodestyle, long lines.

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -9,8 +9,10 @@ def normalize(X, m, s):
         X: numpy.ndarray of shape (d, nx) to normalize
             d: number of data points
             nx: number of features
-        m: numpy.ndarray of shape (nx,) that contains the mean of all features of X
-        s: numpy.ndarray of shape (nx,) that contains the standard deviation of all features of X
+        m: numpy.ndarray of shape (nx,)...
+        ...that contains the mean of all features of X
+        s: numpy.ndarray of shape (nx,) that contains...
+        ...the standard deviation of all features of X
     Returns: The normalized X matrix
     """
     X_norm = (X - m) / s
