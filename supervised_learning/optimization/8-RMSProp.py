ad5bc0fb36fa07f09be97b6e50a85543459aa635
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-26 20:23:37 -0500

    [Creates]: a training operation.

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module for 8-RMSProp.py
+"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """
+    Creates the training operation for a neural network in tensorflow using
+    the RMSProp optimization algorithm.
+
+    Args:
+        loss: loss of the network
+        alpha: learning rate
+        beta2: RMSProp weight
+        epsilon: small number to avoid division by zero
+
+    Returns:
+        The RMSProp optimization operation
+    """
+    return tf.train.RMSPropOptimizer(alpha, beta2, epsilon=epsilon).minimize(loss)
