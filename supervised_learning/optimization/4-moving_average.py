e258fa2d774345998db90aeb0ec7bc72647968b8
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-26 20:02:06 -0500

    [Added]: a function that calculates an avg.

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module for task 4-moving_average.py
+"""
+def moving_average(data, beta):
+    """
+    Function that calculates the weighted moving average of a data set
+    """
+    V = 0
+    moving_avg = []
+    for i in range(len(data)):
+        V = beta * V + (1 - beta) * data[i]
+        moving_avg.append(V / (1 - beta ** (i + 1)))
+    return moving_avg
