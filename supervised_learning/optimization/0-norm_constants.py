631d7f7dc001989ca98b3d152a66a2b4fdc1d3bd
LW068 <105394574+LW068@users.noreply.github.com>
2023-06-26 19:18:56 -0500

    [Calculates]: the normalization constants of a matrix.

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def normalization_constants(X):
+    """
+    Calculates the normalization constants of a matrix
+    Args:
+        X: numpy.ndarray of shape (m, nx) to normalize
+            m: number of data points
+            nx: number of features
+    Returns: the mean and standard deviation of each feature
+    """
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    return mean, std
