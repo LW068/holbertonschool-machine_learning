c7de6de38131d95cc574fbb9a6d54b86547c8d0b
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 00:32:36 -0500

    [Fixed]: pycodestyle, long line.

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -9,7 +9,8 @@ def convolve_grayscale_valid(images, kernel):
 
     Args:
         images: numpy.ndarray with shape (m, h, w) containing grayscale images
-        kernel: numpy.ndarray with shape (kh, kw) containing the convolution kernel
+        kernel: numpy.ndarray with shape (kh, kw)...
+        ...containing the convolution kernel
 
     Returns:
         numpy.ndarray containing the convolved images
@@ -23,6 +24,8 @@ def convolve_grayscale_valid(images, kernel):
     for i in range(conv_h):
         for j in range(conv_w):
             image_patch = images[:, i:i+kh, j:j+kw]
-            convolved_images[:, i, j] = np.sum(image_patch * kernel, axis=(1, 2))
+            convolved_images[:, i, j] = np.sum(
+                image_patch * kernel, axis=(1, 2)
+            )
 
     return convolved_images
