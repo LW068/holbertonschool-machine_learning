0f1d788c45a3d5cc5a8c0e715225cdae3abdbd9b
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 14:51:22 -0500

    [Fixed]: pycodestyle, long lines.

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -12,10 +12,14 @@ def train_model(network, data, labels, batch_size,
     data is a numpy.ndarray of shape (m, nx) containing the input data
     labels is a one-hot numpy.ndarray of shape...
     ...(m, classes) containing the labels of data
-    batch_size is the size of the batch used for mini-batch gradient descent
-    epochs is the number of passes through data for mini-batch gradient descent
-    verbose is a boolean that determines if output should be printed during training
-    shuffle is a boolean that determines whether to shuffle the batches every epoch
+    batch_size is the size of the batch...
+    ...used for mini-batch gradient descent
+    epochs is the number of passes through data for...
+    ...mini-batch gradient descent
+    verbose is a boolean that determines if output...
+    ...should be printed during training
+    shuffle is a boolean that determines whether to...
+    ...shuffle the batches every epoch
     Returns: the History object generated after training the model
     """
     
