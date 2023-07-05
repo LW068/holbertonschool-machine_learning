7be75489cfc5b799787d68dad0e1fba5f2e93abf
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 00:34:34 -0500

    [Added]: convolve_grayscale_same

---
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""convolve_grayscale_same"""
+import numpy as np
+
+def convolve_grayscale_same(images, kernel):
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    # Calculate the padding size
+    pad_h = kh // 2
+    pad_w = kw // 2
+
+    # Create an array with zero padding
+    padded_images = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), 'constant')
+
+    # Initialize an array for the convolved images
+    convolved_images = np.zeros((m, h, w))
+
+    # Perform the convolution
+    for i in range(h):
+        for j in range(w):
+            image_patch = padded_images[:, i:i+kh, j:j+kw]
+            convolved_images[:, i, j] = np.sum(image_patch * kernel, axis=(1, 2))
+
+    return convolved_images
+
