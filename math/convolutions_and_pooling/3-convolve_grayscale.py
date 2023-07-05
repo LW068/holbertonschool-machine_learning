e21e1babcf61129ac1a7c54ae6573fa507572afb
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 15:54:21 -0500

    [Added]: convolve grayscael

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -21,26 +21,21 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kh, kw = kernel.shape
     sh, sw = stride
 
-    # Calculate the output shape based on padding type
+    # Calculate padding based on 'same', 'valid' or custom
     if padding == 'same':
-        ph = int(np.ceil((h - kh + 1) / sh))
-        pw = int(np.ceil((w - kw + 1) / sw))
-        pad_h = max((ph - 1) * sh + kh - h, 0)
-        pad_w = max((pw - 1) * sw + kw - w, 0)
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
     elif padding == 'valid':
-        ph, pw = 0, 0
-        pad_h, pad_w = 0, 0
+        ph = pw = 0
     else:
         ph, pw = padding
-        pad_h = max((h - kh + 2 * ph) % sh, 0)
-        pad_w = max((w - kw + 2 * pw) % sw, 0)
 
     # Create an array with zero padding
-    padded_images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    padded_images = np.pad(images, ((0,0), (ph, ph), (pw, pw)), 'constant')
 
     # Calculate the output shape
-    h_prime = int((h + 2 * ph - kh + pad_h) / sh + 1)
-    w_prime = int((w + 2 * pw - kw + pad_w) / sw + 1)
+    h_prime = (h - kh + 2 * ph) // sh + 1
+    w_prime = (w - kw + 2 * pw) // sw + 1
 
     # Initialize an array for the convolved images
     convolved_images = np.zeros((m, h_prime, w_prime))
