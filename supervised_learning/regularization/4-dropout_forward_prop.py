2e215943345de936af48d683c0e1033de6a59c1d
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 13:53:56 -0500

    [Added]: a fcuntionfor forward prop

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python3
 """
-Module that contains a function for forward propagation with dropout
+Module for function dropout_forward_prop
 """
+
 import numpy as np
 
 
 def dropout_forward_prop(X, weights, L, keep_prob):
     """
-    Conducts forward propagation using Dropout
+    Conducts forward propagation using Dropout.
+    
+    X is a numpy.ndarray of shape (nx, m) containing the input data for the network
+    nx is the number of input features
+    m is the number of data points
+    weights is a dictionary of the weights and biases of the neural network
+    L the number of layers in the network
+    keep_prob is the probability that a node will be kept
+    All layers except the last should use the tanh activation function
+    The last layer should use the softmax activation function
 
-    Args:
-        X: numpy.ndarray of shape (nx, m) containing the input data
-           nx: number of input features
-           m: number of data points
-        weights: dictionary of the weights and biases of the neural network
-        L: the number of layers in the network
-        keep_prob: probability that a node will be kept
-
-    Returns:
-        dictionary containing the outputs of each layer and the dropout
-        mask used on each layer
+    Returns: a dictionary containing the outputs of each layer and the dropout
+             mask used on each layer
     """
+    
     cache = {}
-    cache["A0"] = X
-    for layer in range(1, L + 1):
-        A_prev = cache["A" + str(layer - 1)]
-        W = weights["W" + str(layer)]
-        b = weights["b" + str(layer)]
-        Z = np.matmul(W, A_prev) + b
-        if layer != L:
+    cache['A0'] = X
+
+    for i in range(L):
+        # Linear Transformation
+        Z = np.matmul(weights['W' + str(i + 1)], cache['A' + str(i)]) + weights['b' + str(i + 1)]
+
+        # Activation
+        if i == L - 1:
+            # Softmax activation for the last layer
+            A = np.exp(Z) / np.sum(np.exp(Z), axis=0)
+        else:
+            # tanh activation for the other layers
             A = np.tanh(Z)
-            D = np.random.rand(A.shape[0], A.shape[1])
-            D = np.where(D < keep_prob, 1, 0)
+            # Dropout
+            D = np.random.binomial(1, keep_prob, size=A.shape)
             A *= D
             A /= keep_prob
-            cache["D" + str(layer)] = D
-        else:
-            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
-        cache["A" + str(layer)] = A
-        cache["Z" + str(layer)] = Z
+            cache['D' + str(i + 1)] = D
+
+        cache['A' + str(i + 1)] = A
+
     return cache
