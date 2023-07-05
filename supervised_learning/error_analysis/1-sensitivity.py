af6985132d78eda3d874006231636b8e0c8ce6bd
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-04 22:43:28 -0500

    [Added]: calcuates the sens

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Module for calculating sensitivity."""
+import numpy as np
+
+
+def sensitivity(confusion):
+    """
+    Calculates the sensitivity for each class in a confusion matrix.
+
+    Args:
+    confusion : np.ndarray of shape (classes, classes) where row indices
+                represent the correct labels and column indices represent the predicted labels.
+    classes : int, the number of classes
+
+    Returns:
+    np.ndarray of shape (classes,) containing the sensitivity of each class.
+    """
+    tp = np.diagonal(confusion)
+    fn = np.sum(confusion, axis=1) - tp
+    sensitivity = tp / (tp + fn)
+
+    return sensitivity
