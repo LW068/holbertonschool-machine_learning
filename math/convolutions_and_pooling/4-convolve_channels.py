c1d498737850bb8a2b30fa43a375d983cf194a33
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 16:01:00 -0500

    [Fixed]: pycodestyle, blank line

--- math/convolutions_and_pooling/4-convolve_channels.py
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -2,15 +2,19 @@
 """convolve_channels"""
 import numpy as np
 
+
 def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     """
     Function that performs a convolution on images with channels.
 
     Parameters:
-    - images (numpy.ndarray): with shape (m, h, w, c) containing multiple images
-    - kernel (numpy.ndarray): with shape (kh, kw, c) containing the kernel for
+    - images (numpy.ndarray): with shape...
+    ...(m, h, w, c) containing multiple images
+    - kernel (numpy.ndarray): with shape...
+    ...(kh, kw, c) containing the kernel for
                               the convolution
-    - padding (tuple or str): either a tuple of (ph, pw), ‘same’, or ‘valid’
+    - padding (tuple or str): either a...
+    ...tuple of (ph, pw), ‘same’, or ‘valid’
     - stride (tuple): of (sh, sw)
 
     Returns:
@@ -29,7 +33,9 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     else:
         ph, pw = padding
 
-    pad_images = np.pad(images, ((0,0), (ph, ph), (pw, pw), (0, 0)), 'constant')
+    pad_images = np.pad(images, ((0, 0),
+                                 (ph, ph), (pw, pw),
+                                 (0, 0)), 'constant')
 
     output_h = (h - kh + 2 * ph) // sh + 1
     output_w = (w - kw + 2 * pw) // sw + 1
@@ -39,6 +45,6 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     for i in range(output_h):
         for j in range(output_w):
             output[:, i, j] = (pad_images[:, i*sh: i*sh + kh, j*sw: j*sw + kw,
-                                          :] * kernel).sum(axis=(1,2,3))
+                                          :] * kernel).sum(axis=(1, 2, 3))
             
     return output
