260faf8169559714588acb74a8a1d5ceae8e5bda
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-05 15:15:43 -0500

    [Fixed]: pycodestyle, long lines.

--- supervised_learning/keras/7-train.py
+++ supervised_learning/keras/7-train.py
@@ -5,15 +5,11 @@ Module to train model with learning rate decay
 import tensorflow.keras as K
 
 
-def learning_rate(epoch):
-    """ updates the learning rate using inverse time decay"""
-    return alpha / (1 + decay_rate * epoch)
+def train_model(network, data, labels, batch_size, epochs, validation_data=None, early_stopping=False, patience=0, learning_rate_decay=False, alpha=0.1, decay_rate=1, verbose=True, shuffle=False):
 
-
-def train_model(network, data, labels, batch_size, epochs,
-                validation_data=None, early_stopping=False, patience=0,
-                learning_rate_decay=False, alpha=0.1, decay_rate=1,
-                verbose=True, shuffle=False):
+    def learning_rate(epoch):
+        """ updates the learning rate using inverse time decay"""
+        return alpha / (1 + decay_rate * epoch)
     """
     Function that trains a model using mini-batch gradient descent
 
@@ -45,14 +41,11 @@ def train_model(network, data, labels, batch_size, epochs,
         if early_stopping:
             callbacks.append(K.callbacks.EarlyStopping(monitor='val_loss',
                                                        patience=patience))
-
         if learning_rate_decay:
-            callbacks.append(K.callbacks.LearningRateScheduler(learning_rate,
-                                                               verbose=1))
+            callbacks.append(K.callbacks.LearningRateScheduler(learning_rate, verbose=1))
 
-    history = network.fit(data, labels, batch_size=batch_size,
-                          epochs=epochs, verbose=verbose, shuffle=shuffle,
-                          validation_data=validation_data,
-                          callbacks=callbacks)
+    history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
+                          validation_data=validation_data, callbacks=callbacks,
+                          verbose=verbose, shuffle=shuffle)
 
     return history
