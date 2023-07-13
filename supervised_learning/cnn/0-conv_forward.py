6bab2ad7eb4e4cc60ca859d09fbcf67e30569c4a
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-12 20:50:56 -0500

    [Added]: excpetion

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -23,36 +23,38 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     Returns:
     The output of the convolutional layer
     """
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, c_prev, c_new = W.shape
-    sh, sw = stride
+    try:
+        m, h_prev, w_prev, c_prev = A_prev.shape
+        kh, kw, c_prev, c_new = W.shape
+        sh, sw = stride
 
-    if padding == "same":
-        ph = max((h_prev - 1) * sh + kh - h_prev, 0) // 2
-        pw = max((w_prev - 1) * sw + kw - w_prev, 0) // 2
-    else:
-        ph = pw = 0
+        if padding == "same":
+            ph = max((h_prev - 1) * sh + kh - h_prev, 0) // 2
+            pw = max((w_prev - 1) * sw + kw - w_prev, 0) // 2
+        else:
+            ph = pw = 0
 
-    h_new = (h_prev - kh + 2 * ph) // sh + 1
-    w_new = (w_prev - kw + 2 * pw) // sw + 1
+        h_new = (h_prev - kh + 2 * ph) // sh + 1
+        w_new = (w_prev - kw + 2 * pw) // sw + 1
 
-    A = np.zeros((m, h_new, w_new, c_new))
+        A = np.zeros((m, h_new, w_new, c_new))
 
-    A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph),
-                                 (pw, pw), (0, 0)), mode='constant')
+        A_prev_pad = np.pad(A_prev, ((0, 0), (ph, ph),
+                                     (pw, pw), (0, 0)), mode='constant')
 
-    for i in range(m):
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    h_start = h * sh
-                    h_end = h_start + kh
-                    w_start = w * sw
-                    w_end = w_start + kw
-                    A_slice = A_prev_pad[i, h_start:h_end, w_start:w_end]
-                    weights = W[:, :, :, c]
-                    biases = b[0, 0, 0, c]
-                    A[i, h, w, c] = activation(np.sum(A_slice * weights)
-                                               + biases)
+        for i in range(m):
+            for h in range(h_new):
+                for w in range(w_new):
+                    for c in range(c_new):
+                        h_start = h * sh
+                        h_end = h_start + kh
+                        w_start = w * sw
+                        w_end = w_start + kw
+                        A_slice = A_prev_pad[i, h_start:h_end, w_start:w_end]
+                        weights = W[:, :, :, c]
+                        biases = b[0, 0, 0, c]
+                        A[i, h, w, c] = activation(np.sum(A_slice * weights) + biases)
 
-    return A
+        return A
+    except Exception as e:
+        print("Exception occurred:", e)
