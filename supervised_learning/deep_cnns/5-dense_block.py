43556b05cb73e6c025c27da7939df65fb1d92b88
LW068 <105394574+LW068@users.noreply.github.com>
2023-07-19 19:03:26 -0500

    [Fixed]: pycodestyle, white spaces

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""" 
-Module to define the Dense Block 
+"""
+Module to define the Dense Block
 based on Densely Connected Convolutional Networks
 """
 
@@ -9,7 +9,8 @@ import tensorflow.keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """
-    Builds a dense block as described in Densely Connected Convolutional Networks:
+    Builds a dense block as described in Densely
+    Connected Convolutional Networks:
 
     - X is the output from the previous layer
     - nb_filters is an integer representing the number of filters in X
@@ -17,7 +18,7 @@ def dense_block(X, nb_filters, growth_rate, layers):
     - layers is the number of layers in the dense block
 
     Returns:
-    The concatenated output of each layer within the Dense Block 
+    The concatenated output of each layer within the Dense Block
     and the number of filters within the concatenated outputs, respectively
     """
     for i in range(layers):
@@ -25,14 +26,17 @@ def dense_block(X, nb_filters, growth_rate, layers):
         X_copy = X
         X = K.layers.BatchNormalization(axis=3)(X)
         X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(4 * growth_rate, (1, 1), padding='same', kernel_initializer='he_normal')(X)
+        X = K.layers.Conv2D(4 * growth_rate, (1, 1),
+                            padding='same', kernel_initializer='he_normal')(X)
 
         # Batch Normalization -> ReLU -> 3x3 convolution
         X = K.layers.BatchNormalization(axis=3)(X)
         X = K.layers.Activation('relu')(X)
-        X = K.layers.Conv2D(growth_rate, (3, 3), padding='same', kernel_initializer='he_normal')(X)
+        X = K.layers.Conv2D(growth_rate, (3, 3),
+                            padding='same', kernel_initializer='he_normal')(X)
 
-        # Concatenate the output of the 3x3 convolution with the inputs for the next layer
+        # Concatenate the output of the 3x3
+        # convolution with the inputs for the next layer
         X = K.layers.concatenate([X_copy, X])
         nb_filters += growth_rate
 
