f07c98cb107fa81ce6f8c22dd9fe0fb198ed531f
LW068 <105394574+LW068@users.noreply.github.com>
2023-08-21 16:56:07 -0500

    pycodestyle  2-yolo.py

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -69,16 +69,19 @@ class Yolo:
                             (by + bh / 2) * image_size[0]
                         )
 
-                        tx[cy, cx, b], ty[cy, cx, b], tw[cy, cx, b], th[cy, cx, b] = (
+                        tx[cy, cx, b], ty[cy, cx, b],
+                        tw[cy, cx, b], th[cy, cx, b] = (
                             x1, y1, x2, y2
                         )
+
 
             boxes.append(np.concatenate((tx, ty, tw, th), axis=-1))
 
         return boxes, confidences, class_probs
 
     def filter_boxes(self, boxes, box_confidences, box_class_probs):
-        """Refine and filter boxes based on confidence and class probability."""
+        """Refine and filter boxes based on
+        confidence and class probability."""
         box_scores = [
             conf * prob for conf, prob in zip(box_confidences, box_class_probs)
         ]
