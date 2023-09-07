695cab2330a52717fa33a0ddc99db7ec318eb7d8
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-07 06:37:48 -0500

    Update 1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """
-This module contains a function that calculates the minor matrix of a matrix.
+This module contains functions that calculate the minor matrix and determinant of a matrix.
 """
 
 def determinant(matrix):
+    """
+    Calculates the determinant of a matrix.
+    """
     if len(matrix) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
@@ -17,15 +20,18 @@ def determinant(matrix):
 
 
 def minor(matrix):
-    """Calculates the minor matrix of a matrix."""
-    if not isinstance(matrix, list) or \
-            not all(isinstance(row, list) for row in matrix) or \
-            len(matrix) == 0 or len(matrix) != len(matrix[0]):
+    """
+    Calculates the minor matrix of a matrix.
+    """
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 0 or len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a non-empty square matrix")
-    
+
     if len(matrix) == 1:
         return [[1]]
-    
+
     minor_matrix = []
     for i in range(len(matrix)):
         minor_row = []
@@ -34,27 +40,5 @@ def minor(matrix):
             minor_value = determinant(sub_matrix)
             minor_row.append(minor_value)
         minor_matrix.append(minor_row)
-  
+
     return minor_matrix
-
-
-if __name__ == '__main__':
-    mat1 = [[5]]
-    mat2 = [[1, 2], [3, 4]]
-    mat3 = [[1, 1], [1, 1]]
-    mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
-    mat5 = []
-    mat6 = [[1, 2, 3], [4, 5, 6]]
-
-    print(minor(mat1))
-    print(minor(mat2))
-    print(minor(mat3))
-    print(minor(mat4))
-    try:
-        minor(mat5)
-    except Exception as e:
-        print(e)
-    try:
-        minor(mat6)
-    except Exception as e:
-        print(e)
