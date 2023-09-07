049058f1614406675b0336aa932200cbe5c54f32
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-07 03:51:34 -0500

    Update 0-determinant.py

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,32 +1,30 @@
 #!/usr/bin/env python3
-import numpy as np
-
-
 def determinant(matrix):
-    """
-    Calculates the determinant of a matrix.
-    """
-    # Check if matrix is a list of lists
+    """Calculates the determinant of a matrix."""
     if type(matrix) is not list or not all(type(row) is list for row in matrix):
         raise TypeError("matrix must be a list of lists")
     
-    # Check if matrix is square
     if len(matrix) == 0:
         return 1
+    
     if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
     
-    # Calculate determinant for 0x0, 1x1 and 2x2 matrices directly
     if len(matrix) == 1:
         return matrix[0][0]
+    
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     
-    # Use NumPy for larger matrices
-    return int(round(np.linalg.det(np.array(matrix))))
+    det = 0
+    for i in range(len(matrix)):
+        minor = [row[:i] + row[i+1:] for row in matrix[1:]]
+        cofactor = (-1) ** i * matrix[0][i]
+        det += cofactor * determinant(minor)
+    
+    return det
 
 if __name__ == '__main__':
-    # Test cases
     mat0 = [[]]
     mat1 = [[5]]
     mat2 = [[1, 2], [3, 4]]
@@ -35,18 +33,16 @@ if __name__ == '__main__':
     mat5 = []
     mat6 = [[1, 2, 3], [4, 5, 6]]
 
-    print(determinant(mat0))  # Output should be 1
-    print(determinant(mat1))  # Output should be 5
-    print(determinant(mat2))  # Output should be -2
-    print(determinant(mat3))  # Output should be 0
-    print(determinant(mat4))  # Output should be 192
-
+    print(determinant(mat0))
+    print(determinant(mat1))
+    print(determinant(mat2))
+    print(determinant(mat3))
+    print(determinant(mat4))
     try:
-        print(determinant(mat5))  # Should raise TypeError
+        determinant(mat5)
     except Exception as e:
         print(e)
-
     try:
-        print(determinant(mat6))  # Should raise ValueError
+        determinant(mat6)
     except Exception as e:
         print(e)
