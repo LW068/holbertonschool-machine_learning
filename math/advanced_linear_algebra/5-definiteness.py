8b7474255f9044d1845184c3f81bb99b92b43ebb
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-07 08:24:59 -0500

    Update 5-definiteness.py

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -7,16 +7,16 @@ import numpy as np
 
 def definiteness(matrix):
     """Calculates the definiteness of a matrix.
-    
+
     Args:
         matrix (numpy.ndarray): The matrix to check.
-        
+    
     Returns:
         str: The type of definiteness or None.
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
-    
+
     if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
         return None
 
@@ -24,7 +24,7 @@ def definiteness(matrix):
         return None
 
     eigenvalues = np.linalg.eigvals(matrix)
-    
+
     if np.all(eigenvalues > 0):
         return "Positive definite"
     elif np.all(eigenvalues >= 0):
