e973d1b62d8be0068c4185f9997d83cc34571537
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-07 07:00:12 -0500

    Update 1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -26,7 +26,7 @@ def minor(matrix):
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
     
-    if len(matrix) == 0 or len(matrix) != len(matrix[0]):
+    if len(matrix) == 0 or any(len(row) != len(matrix) for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
     if len(matrix) == 1:
