9297008833f6bb43344a9e1e506a9087b757360f
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-07 07:15:48 -0500

    Update 2-cofactor.py

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -4,6 +4,56 @@ Calculates the cofactor matrix of a given matrix.
 """
 
 
+def determinant(matrix):
+    """Calculates the determinant of a matrix."""
+    if not isinstance(matrix, list) or \
+            not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if len(matrix) == 0 or len(matrix[0]) == 0:
+        return 1
+
+    if len(matrix) != len(matrix[0]):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+
+    if len(matrix) == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for i in range(len(matrix)):
+        minor = [row[:i] + row[i+1:] for row in matrix[1:]]
+        cofactor = (-1) ** i * matrix[0][i]
+        det += cofactor * determinant(minor)
+
+    return det
+
+
+if __name__ == '__main__':
+    mat0 = [[]]
+    mat1 = [[5]]
+    mat2 = [[1, 2], [3, 4]]
+    mat3 = [[1, 1], [1, 1]]
+    mat4 = [[5, 7, 9], [3, 1, 8], [6, 2, 4]]
+    mat5 = []
+    mat6 = [[1, 2, 3], [4, 5, 6]]
+
+    print(determinant(mat0))
+    print(determinant(mat1))
+    print(determinant(mat2))
+    print(determinant(mat3))
+    print(determinant(mat4))
+    try:
+        determinant(mat5)
+    except Exception as e:
+        print(e)
+    try:
+        determinant(mat6)
+    except Exception as e:
+        print(e)
+
 def cofactor(matrix):
     """
     Calculates the cofactor matrix.
