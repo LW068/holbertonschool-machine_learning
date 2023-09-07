385793aaa7b97b21411a5f67be8eedad2ab093eb
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-07 03:59:49 -0500

    Update 0-determinant.py

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -9,29 +9,26 @@ def determinant(matrix):
     if not isinstance(matrix, list) or \
             not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    
-    if len(matrix) == 0:
+
+    if len(matrix) == 0 or len(matrix[0]) == 0:
         return 1
-    
+
     if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    
+
     if len(matrix) == 1:
-        if len(matrix[0]) == 0:
-            return 1
         return matrix[0][0]
-    
+
     if len(matrix) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-    
+
     det = 0
     for i in range(len(matrix)):
         minor = [row[:i] + row[i+1:] for row in matrix[1:]]
         cofactor = (-1) ** i * matrix[0][i]
         det += cofactor * determinant(minor)
-    
-    return det
 
+    return det
 
 if __name__ == '__main__':
     mat0 = [[]]
