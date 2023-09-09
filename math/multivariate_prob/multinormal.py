18549b65e73ddc65130c797a646daf6b37677d2c
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-09 03:31:01 -0500

    Update multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -29,13 +29,47 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = np.dot(data - self.mean, (data - self.mean).T) / (n - 1)
 
+    
+    def pdf(self, x):
+        """Calculate the PDF at a data point.
+
+        Parameters:
+            x (numpy.ndarray): shape (d, 1) data point for PDF calculation.
+
+        Returns:
+            float: value of the PDF.
+        """
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        d = self.mean.shape[0]
+        if x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+
+        x_m = x - self.mean
+        cov_inv = np.linalg.inv(self.cov)
+        cov_det = np.linalg.det(self.cov)
+
+        pdf = (1. / (np.sqrt((2 * np.pi) ** d * cov_det)) *
+               np.exp(-(np.linalg.solve(self.cov, x_m).T.dot(x_m)) / 2))
+
+        return pdf.flatten()[0]
+
 
 if __name__ == '__main__':
     import numpy as np
 
     np.random.seed(0)
     data = np.random.multivariate_normal(
-        [12, 30, 10], [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 10000).T
+        [12, 30, 10], 
+        [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 
+        10000
+    ).T
     mn = MultiNormal(data)
-    print(mn.mean)
-    print(mn.cov)
+    x = np.random.multivariate_normal(
+        [12, 30, 10], 
+        [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 
+        1
+    ).T
+    print(x)
+    print(mn.pdf(x))
