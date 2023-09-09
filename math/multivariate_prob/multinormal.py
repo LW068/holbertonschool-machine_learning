14d0d90c5a624a16728f166049c935f73ee76109
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-09 03:33:22 -0500

    Update multinormal.py

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -8,17 +8,7 @@ class MultiNormal:
     """Class that represents a Multivariate Normal distribution."""
 
     def __init__(self, data):
-        """
-        Initialize MultiNormal instance.
-
-        Parameters:
-        - data (numpy.ndarray of shape (d, n)): The data set.
-
-        Sets the public instance variables:
-        - mean (numpy.ndarray of shape (d, 1)): The mean of the data set.
-        - cov (numpy.ndarray of shape (d, d)):...
-        ...The covariance matrix of the data set.
-        """
+        """Initialize MultiNormal instance."""
         if not isinstance(data, np.ndarray) or len(data.shape) != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
 
@@ -29,16 +19,8 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         self.cov = np.dot(data - self.mean, (data - self.mean).T) / (n - 1)
 
-    
     def pdf(self, x):
-        """Calculate the PDF at a data point.
-
-        Parameters:
-            x (numpy.ndarray): shape (d, 1) data point for PDF calculation.
-
-        Returns:
-            float: value of the PDF.
-        """
+        """Calculate the PDF at a data point."""
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
 
@@ -57,18 +39,16 @@ class MultiNormal:
 
 
 if __name__ == '__main__':
-    import numpy as np
-
     np.random.seed(0)
     data = np.random.multivariate_normal(
-        [12, 30, 10], 
-        [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 
+        [12, 30, 10],
+        [[36, -30, 15], [-30, 100, -20], [15, -20, 25]],
         10000
     ).T
     mn = MultiNormal(data)
     x = np.random.multivariate_normal(
-        [12, 30, 10], 
-        [[36, -30, 15], [-30, 100, -20], [15, -20, 25]], 
+        [12, 30, 10],
+        [[36, -30, 15], [-30, 100, -20], [15, -20, 25]],
         1
     ).T
     print(x)
