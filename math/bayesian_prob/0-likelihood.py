02dc6513e508331304d6a7332768e1ca34c1afa2
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-10 06:29:43 -0500

    Update 0-likelihood.py

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Bayesian Probability - Likelihood"""
 
-
 import numpy as np
 from scipy.special import comb
 
@@ -10,29 +9,18 @@ def likelihood(x, n, P):
     """
     Calculate the likelihood of obtaining this data given various
     hypothetical probabilities of developing severe side effects.
-
-    Parameters:
-    - x (int): number of patients with severe side effects
-    - n (int): total number of patients observed
-    - P (1D numpy.ndarray): hypothetical probabilities of side effects
-
-    Returns:
-    - 1D numpy.ndarray: likelihood of obtaining the data, x and n,
-                         for each probability in P
     """
-    # Validate the inputs
     if type(n) is not int or n <= 0:
         raise ValueError("n must be a positive integer")
     if type(x) is not int or x < 0:
-        raise ValueError("x must be an integer >= 0")
+        raise ValueError("x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if type(P) is not np.ndarray or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if np.any((P < 0) | (P > 1)):
-        raise ValueError("All values in P must be in [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
 
-    # Calculate the likelihood
     binom_coeff = comb(n, x)
     likelihood_values = binom_coeff * (P ** x) * ((1 - P) ** (n - x))
 
