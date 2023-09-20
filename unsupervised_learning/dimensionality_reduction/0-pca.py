3e3abd0856fc510359f8b3f09337b7fba4840bcf
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-20 18:58:52 -0500

    Update 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -2,6 +2,7 @@
 """PCA on a dataset"""
 import numpy as np
 
+
 def pca(X, var=0.95):
     """PCA on a dataset"""
     # calculatign the covariance matrix
@@ -19,11 +20,12 @@ def pca(X, var=0.95):
     cumul_eigenvalues = np.cumsum(sorted_eigenvalues)
     total_eigenvalues = np.sum(sorted_eigenvalues)
     threshold_value = total_eigenvalues * var
-    
+
     # finding the number of principal components needed
     num_components = np.argmax(cumul_eigenvalues >= threshold_value) + 1
-    
+    print("Number of components:", num_components)
+
     # constructed the weights matrix "W"
     W = sorted_eigenvectors[:, :num_components]
-    
+
     return W
