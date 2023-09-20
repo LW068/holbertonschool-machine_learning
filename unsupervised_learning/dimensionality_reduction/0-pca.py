fda21882f94b49c724f3fa716597a39bdaaaae5d
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-19 21:32:09 -0500

    [Fixed]: pycodestyle 0-pca.py

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -2,29 +2,29 @@
 """PCA on a dataset"""
 import numpy as np
 
+
 def pca(X, var=0.95):
-  """PCA on a dataset"""
-  # calculatign the covariance matrix
-  cov_matrix = np.cov(X, rowvar=False)
+    """PCA on a dataset"""
+    # calculatign the covariance matrix
+    cov_matrix = np.cov(X, rowvar=False)
 
-  # performign eigendecomposition
-  eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+    # performign eigendecomposition
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
 
-  # sorting the  eigenvalues and the corresponding eigenvectors
-  sorted_indices = np.argsort(eigenvalues)[::-1]
-  sorted_eigenvalues = eigenvalues[sorted_indices]
-  sorted_eigenvectors = eigenvectors[:, sorted_indices]
+    # sorting the eigenvalues and the corresponding eigenvectors
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvalues = eigenvalues[sorted_indices]
+    sorted_eigenvectors = eigenvectors[:, sorted_indices]
 
-  # clalculating the cumulative sum of eigenvalues to maintain the given variance
-  cumul_eigenvalues = np.cumsum(sorted_eigenvalues)
-  total_eigenvalues = np.sum(sorted_eigenvalues)
-  threshold_value = total_eigenvalues * var
-    
-  # finding the number of principal components needed
-  num_components = np.argmax(cumul_eigenvalues >= threshold_value) + 1
-    
-  # constructed the weights matrix "W"
-  W = sorted_eigenvectors[:, :num_components]
-    
+    # clalculating the cumulative sum of eigenvalues to maintain the given variance
+    cumul_eigenvalues = np.cumsum(sorted_eigenvalues)
+    total_eigenvalues = np.sum(sorted_eigenvalues)
+    threshold_value = total_eigenvalues * var
+
+    # finding the number of principal components needed
+    num_components = np.argmax(cumul_eigenvalues >= threshold_value) + 1
+
+    # constructed the weights matrix "W"
+    W = sorted_eigenvectors[:, :num_components]
+
 return W
-
