3ed4bcbb7595e2cd2202a83d4b9f612b89af99f6
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-26 13:26:09 -0500

    Update 1-pca.py

---
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""PCA on a dataset"""
+import numpy as np
+
+def pca(X, ndim):
+    """Perform PCA on a dataset to reduce it to ndim dimensions."""
+    # cnter the dataset
+    X_centered = X - np.mean(X, axis=0)
+
+    # calculate the covarance matrix
+    cov_matrix = np.cov(X_centered, rowvar=False)
