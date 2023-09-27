c9d06cba482d556378acfd0826e9baabd35b53e9
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-26 23:29:44 -0500

    [Added]: testing 1-kmeans.py

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -36,3 +36,16 @@ def kmeans(X, k, iterations=1000):
         C = new_C
 
     return C, clss
+
+# test the function
+if __name__ == "__main__":
+    np.random.seed(0)
+    a = np.random.multivariate_normal([30, 40], [[16, 0], [0, 16]], size=50)
+    b = np.random.multivariate_normal([10, 25], [[16, 0], [0, 16]], size=50)
+    c = np.random.multivariate_normal([40, 20], [[16, 0], [0, 16]], size=50)
+    d = np.random.multivariate_normal([60, 30], [[16, 0], [0, 16]], size=50)
+    e = np.random.multivariate_normal([20, 70], [[16, 0], [0, 16]], size=50)
+    X = np.concatenate((a, b, c, d, e), axis=0)
+    np.random.shuffle(X)
+    C, clss = kmeans(X, 5)
+    print("Centroids:", C)
