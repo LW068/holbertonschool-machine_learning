8845f5e0723e8666cf574988e39fe10dd422a619
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-26 22:22:26 -0500

    [Added]: x & kparameters in 0-initialize.py

---
+++ unsupervised_learning/clustering/0-initialize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""initialize K-means centroids"""
+import numpy as np
+
+
+def initialize(X, k):
+    """initializes cluster centroids for K-means."""
+
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(k) is not int or k <= 0:
+        return None
