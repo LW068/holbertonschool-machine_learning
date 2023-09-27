f1bd80cdf277ffb0062253bc4121a1f395380c57
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-26 19:12:26 -0500

    Update 1-pca.py

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -5,7 +5,23 @@ import numpy as np
 def pca(X, ndim):
     """Perform PCA on a dataset to reduce it to ndim dimensions."""
     # cnter the dataset
-    X_centered = X - np.mean(X, axis=0)
+    X_centered = X - np.mean(X, axis=0) # is centers the dataset by
+    # subtracting th mean of each feature.
 
     # calculate the covarance matrix
     cov_matrix = np.cov(X_centered, rowvar=False)
+
+    # perform eigendecomposition
+    eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+
+    # sort the eigenvalues and corresponding eigenvectors
+    sorted_indices = np.argsort(eigenvalues)[::-1]
+    sorted_eigenvectors = eigenvectors[:, sorted_indices]
+
+    # select the first ndim eigenvectors
+    W = sorted_eigenvectors[:, :ndim]
+
+    # transform the dataset
+    T = np.dot(X_centered, W)
+
+    return T
