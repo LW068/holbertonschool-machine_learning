c11b5164d1baf2c6e533dd3a9252bc7ac4604a05
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-27 18:39:40 -0500

    Pycodestyle whitespaces 2-variance.py

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -10,18 +10,18 @@ def variance(X, C):
         return None
     if not isinstance(C, np.ndarray) or len(C.shape) != 2:
         return None
-    
+
     try:
         # calculate the distances from each point to each centroid
         distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)
-        
+
         # find the closest centroid f0r each point
         clss = np.argmin(distances, axis=1)
-        
+
         # calculate the variance
         min_distances = np.min(distances, axis=1)
         var = np.sum(min_distances ** 2)
-        
+
         return var
     except Exception as e:
         return None
