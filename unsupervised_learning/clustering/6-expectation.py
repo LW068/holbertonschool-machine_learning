e2e2ce812436fc4d78c5ddcbda0e431451cf5363
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-27 22:53:57 -0500

    [Added]: expectation step for EM in GMM - 6-expectation.py

---
+++ unsupervised_learning/clustering/6-expectation.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""function that calculates the expectation step for GMM"""
+import numpy as np
+
+
+pdf = __import__('5-pdf').pdf
+
+def expectation(X, pi, m, S):
+    """fucntion that calculates the expectation step for GMM"""  
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None, None
+    
+    if not isinstance(pi, np.ndarray) or len(pi.shape) != 1:
+        return None, None
+    
+    if not isinstance(m, np.ndarray) or len(m.shape) != 2:
+        return None, None
+    
+    if not isinstance(S, np.ndarray) or len(S.shape) != 3:
+        return None, None
+    
+    n, d = X.shape
+    k = pi.shape[0]
+    
+    if (k, d) != m.shape or (k, d, d) != S.shape:
+        return None, None
+    
+    g = np.zeros((k, n))
+    
+    for i in range(k):
+        P = pdf(X, m[i], S[i])
+        g[i] = P * pi[i]
+        
+    total = np.sum(g, axis=0, keepdims=True)
+    
+    g /= total
+    
+    l = np.sum(np.log(total))
+    
+    return g, l
