28fad364fa90fc305829a7884bbaa1634e288d7f
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-27 23:58:55 -0500

    Update 8-EM.py

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """PLACEHOLDER FOR NOW - CODE WONT WORK FULLY"""
 
+import numpy as np
 initialize = __import__('4-initialize').initialize
 expectation = __import__('6-expectation').expectation
 maximization = __import__('7-maximization').maximization
+
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """PLACEHOLDER FOR NOW - CODE WONT WORK FULLY"""
