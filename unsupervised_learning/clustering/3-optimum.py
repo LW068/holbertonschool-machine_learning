15638f9b6cab62f3492bd133e717362aa03b9c3b
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-27 21:54:48 -0500

    {Added]: function that tests for # of clusters  3-optimum.py

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""fucntion that tests for the optimum number of clusters"""
+import numpy as np
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """fucntion that tests for the optimum number of clusters"""
+    if kmax is None:
+          kmax = len(X)
+
+    # imports the required functions
+    kmeans = __import__('1-kmeans').kmeans  # still wrong but we'll see...
+    variance = __import__('2-variance').variance
+
+    if kmax < kmin or kmax > len(X) or kmin < 1:
+        return None, None
+
+    results = []
+    d_vars = []
+
+    for k in range(kmin, kmax + 1):
+        C, clss = kmeans(X, k, iterations)
+        if C is None:
+            return None, None
+        results.append((C, clss))
+
+        var = variance(X, C)
+        if k == kmin:
+            smallest_var = var
+
+        d_vars.append(var - smallest_var)
+
+    return results, d_vars
