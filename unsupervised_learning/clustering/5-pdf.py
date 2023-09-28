e8a12be69d81e8e5d20406e85161da1e17c3bd61
LW068 <105394574+LW068@users.noreply.github.com>
2023-09-27 22:36:44 -0500

    [Fixed]: pycodestyle whitepsaces - 5-pdf.py

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -9,18 +9,18 @@ def pdf(X, m, S):
     density function of a Gaussian Distribution"""    
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None
-    
+
     if not isinstance(m, np.ndarray) or len(m.shape) != 1:
         return None
-    
+
     if not isinstance(S, np.ndarray) or len(S.shape) != 2:
         return None
-    
+
     n, d = X.shape
-    
+
     if d != m.shape[0] or (d, d) != S.shape:
         return None
-    
+
     # calculate the probability density function
     # should be simple enough:
     S_inv = np.linalg.inv(S)
@@ -28,5 +28,5 @@ def pdf(X, m, S):
     den = np.sqrt((2 * np.pi) ** d * S_det)
     fac = np.einsum('...k,kl,...l->...', X - m, S_inv, X - m)
     P = np.exp(-fac / 2) / den
-    
+
     return np.maximum(P, 1e-300)
