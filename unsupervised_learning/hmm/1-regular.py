59e3696f3b30eedf7b49c1c1c17a151a26a4c7cd
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-04 18:54:08 -0500

    [Added]: determines a steady state of prob - 1-regular.py

--- unsupervised_learning/hmm/1-regular.py
+++ unsupervised_learning/hmm/1-regular.py
@@ -2,3 +2,19 @@
 """ Hidden Markov Models """
 
 import numpy as np
+
+
+def regular(P):
+    """ 
+    Function to determine the steady state probabilities of a regular markov chain 
+    """
+    try:
+        # clculate eigenvalues and eigenvectors
+        w, v = np.linalg.eig(np.transpose(P))
+        # get the index of eigenvalue 1
+        idx = np.argmin(np.abs(w - 1))
+        # normalize the corresponding eigenvector to get the steady state
+        steady = np.real(v[:, idx] / np.sum(v[:, idx]))
+        return steady[np.newaxis, :]  # shape it as (1, n)
+    except Exception:
+        return None
