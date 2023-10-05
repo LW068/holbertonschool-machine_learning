adca0db24c287647dac376558ed387a961da72d4
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-04 19:12:45 -0500

    [Added]: absorbing chains - should work - 2-absorbing.py

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -2,3 +2,10 @@
 """ Hidden Markov Models """
 
 import numpy as np
+
+
+def absorbing(P):
+    """ Funtion to determine if a mrkov chain is absorbing """
+    if np.any(np.diag(P) == 1): # check if any state is absorbing
+        return True
+    return False
