101ef597f6d3c2bebc055e9174b9aba07db85a07
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-04 19:33:10 -0500

    [Added]: performs the forward algorithm for a hmm - 3-forward.py

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -2,3 +2,18 @@
 """ Hidden Markov Models """
 
 import numpy as np
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """ Perform the forward algorithm for a hidden markov model """
+    T = Observation.shape[0]
+    N = Emission.shape[0]
+    F = np.zeros((N, T))
+    F[:, 0] = Initial.T * Emission[:, Observation[0]]
+
+    for t in range(1, T):
+        for n in range(N):
+            F[n, t] = np.sum(F[:, t - 1] * Transition[:, n] * Emission[n, Observation[t]])
+
+    P = np.sum(F[:, -1])
+    return P, F
