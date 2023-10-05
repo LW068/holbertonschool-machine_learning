42fd01575e85a8ecc7e0b48b0d56c2b6b6d0d31a
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-04 19:56:58 -0500

    [Added]: a backward algo - 5-backward.py

--- unsupervised_learning/hmm/5-backward.py
+++ unsupervised_learning/hmm/5-backward.py
@@ -2,3 +2,16 @@
 """ Hidden Markov Models """
 
 import numpy as np
+
+
+def backward(Observation, Emission, Transition, Initial):
+    """ performs a backward algorithm for hmm """
+    N, M = Emission.shape
+    T = Observation.shape[0]
+    B = np.zeros((N, T))
+    B[:, -1] = 1
+    for t in range(T - 2, -1, -1):
+        for n in range(N):
+            B[n, t] = np.sum(B[:, t + 1] * Transition[n, :] * Emission[:, Observation[t + 1]])
+    P = np.sum(Initial.T * Emission[:, Observation[0]] * B[:, 0])
+    return P, B
