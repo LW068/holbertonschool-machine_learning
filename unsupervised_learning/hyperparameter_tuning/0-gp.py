6911bb662e4c5e46c8f23e2d512583678cc05336
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-11 14:48:25 -0500

    [Fixed]: pep8 error 0-gp.py

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -19,6 +19,7 @@ class GaussianProcess:
     def kernel(self, X1, X2):
         """Calculates the covariance kernel matrix between two matrices"""
         # RBF kernel formula computation
-        sqdist = (np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1)
-                  - 2 * np.dot(X1, X2.T))
+        sqdist = (np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) -
+                  2 * np.dot(X1, X2.T))
+
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
