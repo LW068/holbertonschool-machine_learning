03006374d3353a9570d2943a4452f54f94c909e9
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-17 22:43:10 -0500

    [Fixed]: pep8 - 1-gp.py

--- unsupervised_learning/hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -15,18 +15,20 @@ class GaussianProcess:
 
     def kernel(self, X1, X2):
         """Calculates the covariance kernel matrix between two matrices"""
-        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1)
+        sqdist = sqdist - 2 * np.dot(X1, X2.T)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
 
     def predict(self, X_s):
-        """Predicts the mean and standard deviation of points in a Gaussian process"""
+        """Predicts the mean and standard deviation of
+        points in a Gaussian process"""
         K_s = self.kernel(self.X, X_s)
         K_ss = self.kernel(X_s, X_s)
         K_inv = np.linalg.inv(self.K)
 
         mu_s = K_s.T.dot(K_inv).dot(self.Y)
         mu_s = mu_s.reshape((mu_s.shape[0],))
-        
+ 
         sigma_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
         sigma_s = np.diag(sigma_s)
 
