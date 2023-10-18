36d4ed7c4edefcae484861d70d524c66ceb89ede
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-17 22:59:09 -0500

    [Added]: acquisition method to 4-bayes_opt.py

---
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""bayesian optimization module"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """bayesian optimization classs"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """constructor method"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """calculates the next best sample location using
+        the Expected Improvement acquisition function"""
+        mu, sigma = self.gp.predict(self.X_s)
+        with np.errstate(divide='warn'):
+            Z = (mu - self.gp.Y.max() - self.xsi) / sigma
+            EI = (mu - self.gp.Y.max() - self.xsi) * norm.cdf(Z) + sigma * norm.pdf(Z)
+            EI[sigma == 0.0] = 0.0
+        
+        return self.X_s[np.argmax(EI)], EI
