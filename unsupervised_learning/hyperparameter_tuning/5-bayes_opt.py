a2056332e344928c6079d6868f65e2c361a38eeb
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-18 22:52:21 -0500

    [Added]: acquisition method to 5-bayes_opt.py

---
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""bayesian optimization module"""
+from scipy.stats import norm
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization:
+    """bayesian optimization classs"""
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """constructor method"""
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
+        self.xsi = xsi
+        self.minimize = minimize
+
+    def acquisition(self):
+        """calculates the next best sample location using
+        the Expected Improvement acquisition function"""
+
+        mu, sigma = self.gp.predict(self.X_s)
+        with np.errstate(divide='warn'):
+            Z = (
+                (mu - self.gp.Y.max() - self.xsi) /
+                sigma
+            )
+            EI = (
+                (mu - self.gp.Y.max() - self.xsi) * norm.cdf(Z) +
+                sigma * norm.pdf(Z)
+            )
+
+        return self.X_s[np.argmax(EI)], EI
+
+    def optimize(self, iterations=100):
+        """Optimizes the black-box function"""
+        for _ in range(iterations):
+            X_next, _ = self.acquisition()
+            Y_next = self.f(X_next)
+            if X_next in self.gp.X:
+                break
+            self.gp.update(X_next, Y_next)
+        X_opt = self.gp.X[np.argmax(self.gp.Y)]
+        Y_opt = self.gp.Y.max()
+        return X_opt, Y_opt
