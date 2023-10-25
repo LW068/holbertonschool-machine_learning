436cce56f7ee414c5c335a3106e2eec0cbdd5630
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-25 14:24:47 -0500

    [fixed] pep8 - 1-sparse.py

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -12,8 +12,10 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     x = encoder_input
     for units in hidden_layers:
         x = keras.layers.Dense(units, activation='relu')(x)
-    latent = keras.layers.Dense(latent_dims, activation='relu',
-                                activity_regularizer=keras.regularizers.l1(lambtha))(x)
+    latent = keras.layers.Dense(
+        latent_dims, activation='relu',
+        activity_regularizer=keras.regularizers.l1(lambtha)
+    )(x)
     encoder = keras.Model(encoder_input, latent)
 
     # decodeer
