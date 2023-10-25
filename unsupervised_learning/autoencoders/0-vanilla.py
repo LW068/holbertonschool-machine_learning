458d50729af4a0cb6e7bb7eca9dc4150f889142a
LW068 <105394574+LW068@users.noreply.github.com>
2023-10-25 13:50:10 -0500

    Update 0-vanilla.py

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" creates vanilla autoencoder """
+
+import tensorflow.keras as keras
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """ creates vanilla autoencoder """
