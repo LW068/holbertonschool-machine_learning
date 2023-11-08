a5eb84d804cdfa362692ef5dc50dadf946c8f6f0
LW068 <105394574+LW068@users.noreply.github.com>
2023-11-08 16:30:47 -0600

    [Added]: bidirectional cell forw - 5-bi_forward.py

---
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Bidirectional CEll Forward """
+import numpy as np
+
+
+class BidirectionalCell:
+    """ Bidirectional cell class for an RNN """
+    def __init__(self, i, h, o):
+        """ initializes a bidirectional cell"""
+        self.Whf = np.random.randn(h, i + h)  # weight for forw direction
+        self.Whb = np.random.randn(h, i + h)  # weight for back direction
+        self.Wy = np.random.randn(o, 2 * h)   # weight for outputs
+        self.bhf = np.zeros((1, h))           # bias for forw direction
+        self.bhb = np.zeros((1, h))           # bias for back direction
+        self.by = np.zeros((1, o))            # bais for outputs
+
+    def forward(self, h_prev, x_t):
+        """ alculates the hidden state in the forward direction for one time step"""
+        h_next = np.tanh(np.dot(np.hstack((h_prev, x_t)), self.Whf.T) + self.bhf)
+        return h_next
