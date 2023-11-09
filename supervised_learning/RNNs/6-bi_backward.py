587cff625d66f97ade3ee1a27cf145f095daf60e
LW068 <105394574+LW068@users.noreply.github.com>
2023-11-08 22:10:33 -0600

    [Added]: back prop slightly fixed - 6-bi_backward.py

--- supervised_learning/RNNs/6-bi_backward.py
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -7,42 +7,34 @@ class BidirectionalCell:
     """ Bidirectional cell class for an RNN """
     def __init__(self, i, h, o):
         """ initializes a bidirectional cell"""
-        self.Whf = np.random.randn(h, i + h)  # weight for forw direction
-        self.Whb = np.random.randn(h, i + h)  # weight for back direction
-        self.Wy = np.random.randn(o, 2 * h)   # weight for outputs
-        self.bhf = np.zeros((1, h))           # bias for forw direction
-        self.bhb = np.zeros((1, h))           # bias for back direction
-        self.by = np.zeros((1, o))            # bais for outputs
+        self.Whf = np.random.randn(i + h, h)
+        self.Whb = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(2 * h, o)
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """ alculates the hidden state in the
         forward direction for one time step """
         # concatenating the previous hidden state and the current input
-        concat_h_x = np.hstack((h_prev, x_t))
+        concat_h_x = np.concatenate((h_prev, x_t), axis=1)
 
-        # computing the dot product with the weight transpose
-        whf_dot = np.dot(concat_h_x, self.Whf.T)
-
-        # adding the bias term
-        whf_dot_plus_bias = whf_dot + self.bhf
+        # debugging
+        print("concat_h_x shape: {}".format(concat_h_x.shape))
+        print("self.Whf shape: {}".format(self.Whf.shape))
 
         # aplying the tanh activation function to get the next hidden state
-        h_next = np.tanh(whf_dot_plus_bias)
+        h_next = np.tanh(np.dot(concat_h_x, self.Whf) + self.bhf)
         return h_next
 
     def backward(self, h_next, x_t):
-        """calculates the hidden state in the
-        backward direction for one time step"""
-        # concatenate the next hidden state and the current input
-        concat_h_x = np.hstack((h_next, x_t))
+        """ Calculates the hidden state in
+        the backward direction for one time step """
+        # concatenation of the next hidden state and the current input
+        concat_h_x = np.concatenate((h_next, x_t), axis=1)
 
-        # compute the dot product with the backward weight transpose
-        whb_dot = np.dot(concat_h_x, self.Whb.T)
-
-        # add the backward bias term
-        whb_dot_plus_bias = whb_dot + self.bhb
-
-        # apply the tanh activation function to get the previous hidden state
-        h_prev = np.tanh(whb_dot_plus_bias)
+        # backward direction calculation
+        h_prev = np.tanh(np.dot(concat_h_x, self.Whb) + self.bhb)
 
         return h_prev
