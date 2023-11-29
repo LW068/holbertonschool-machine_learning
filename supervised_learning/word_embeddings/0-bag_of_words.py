fe1502915b1c517df96cb0710ddf616f1240d615
LW068 <ricardocoronaberron@gmail.com>
2023-11-28 18:58:08 -0600

    [Fixed]: pep8 and error

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -3,23 +3,24 @@
 import numpy as np
 import re
 
+
 def bag_of_words(sentences, vocab=None):
     """
     Creates a bag of words embedding matrix.
     """
     # preprocess sentences: lowercase and remove punctuation
-    sentences = [re.sub(r'[^\w\s]', '', sentence.lower()) for sentence in sentences]
-
     if vocab is None:
-        vocab = sorted(set(word for sentence in sentences for word in sentence.split()))
+        words_set = set()
+        for sentence in sentences:
+            words_set.update(re.findall(r'\b\w+\b', sentence.lower()))
+        vocab = sorted(words_set)
 
+    vocab_dict = {word: i for i, word in enumerate(vocab)}
     embeddings = np.zeros((len(sentences), len(vocab)))
-    vocab_dict = {word: idx for idx, word in enumerate(vocab)}
 
-    for idx, sentence in enumerate(sentences):
-        words = sentence.split()
-        for word in words:
+    for i, sentence in enumerate(sentences):
+        for word in re.findall(r'\b\w+\b', sentence.lower()):
             if word in vocab_dict:
-                embeddings[idx, vocab_dict[word]] += 1
+                embeddings[i, vocab_dict[word]] += 1
 
     return embeddings, vocab
