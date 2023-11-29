4fa568798e446fcc9704333f778fbbb1bc00667a
LW068 <ricardocoronaberron@gmail.com>
2023-11-28 20:08:00 -0600

    [Fixed]: get_feature_names

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -11,12 +11,12 @@ def tf_idf(sentences, vocab=None):
     """
     # init TfidfVectorizer with given vocab
     vectorizer = TfidfVectorizer(vocabulary=vocab)
-    
+
     # fit and transform the sentences
     X = vectorizer.fit_transform(sentences)
 
     # get the feature names (vocab)
-    features = vectorizer.get_feature_names_out()
+    features = vectorizer.get_feature_names()
 
     # converting sparse matrix to numpy array
     embeddings = X.toarray()
