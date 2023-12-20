964f61862161cc9c1c7e94e02dfa3f8771d92394
LW068 <ricardocoronaberron@gmail.com>
2023-12-20 14:51:20 -0600

    [Fixed]: 0-qa.py - finally runs

--- supervised_learning/qa_bot/0-qa.py
+++ supervised_learning/qa_bot/0-qa.py
@@ -5,20 +5,19 @@ BERT model from TensorFlow Hub and Transformers library
 """
 
 import tensorflow as tf
-import tensorflow_hub as hub
-from transformers import BertTokenizer
+from transformers import BertTokenizer, TFAutoModelForQuestionAnswering
 
 
 def question_answer(query, passage):
     """Finds an answer to a question from a given passage using BERT"""
     bert_tokenizer = BertTokenizer.from_pretrained(
         'bert-large-uncased-whole-word-masking-finetuned-squad')
-    bert_model = hub.load(
-        "https://tfhub.dev/see--/bert-uncased-tf2-qa/1")
+    bert_model = TFAutoModelForQuestionAnswering.from_pretrained(
+        'bert-large-uncased-whole-word-masking-finetuned-squad')
 
     tokens = bert_tokenizer.encode_plus(
         query, passage, return_tensors='tf', truncation=True, 
-        padding=True, max_length=512)
+        padding='max_length', max_length=512)
     input_ids = tokens["input_ids"]
     attention_mask = tokens["attention_mask"]
     token_type_ids = tokens["token_type_ids"]
@@ -29,10 +28,14 @@ def question_answer(query, passage):
     start_idx = tf.argmax(start_scores, axis=1).numpy()[0]
     end_idx = tf.argmax(end_scores, axis=1).numpy()[0]
 
+    # ensureing end_idx is greater or equal to start_idx
+    if end_idx < start_idx:
+        return None
+
     answer_tokens = bert_tokenizer.convert_ids_to_tokens(
-        input_ids.numpy()[0][start_idx:end_idx + 1])
+        input_ids[0, start_idx:end_idx+1])
     answer = bert_tokenizer.convert_tokens_to_string(answer_tokens)
     # checking if answer is valuid
-    if answer and not answer.strip("[CLS] [SEP]").strip():
+    if not answer or answer.strip("[CLS] [SEP]").strip() == '':
         return None  # No valid answer found
     return answer
